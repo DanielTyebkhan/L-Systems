c005a56e8bcae2330160d9bf1f4aceb85c38ee7f
tyebkhan <30511534+tyebkhan@users.noreply.github.com>
2022-02-24 19:02:09 -0500

    updated main

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 
-from algae import algae
+import sys
+from models.algae import algae
 from render.renderer import local_render, system_to_graphviz
+
+KEYWORD_MAP = {
+    'algae': algae
+
+}
 
 
 def main():
-    system = algae()
-    graph = system_to_graphviz(system, 7, 'Algae')
+    system_in, iterations = sys.argv[1:]
+    system = KEYWORD_MAP[system_in]()
+    graph = system_to_graphviz(system, int(iterations), 'Algae')
     local_render(graph)
+
 
 if __name__ == '__main__':
     main()
